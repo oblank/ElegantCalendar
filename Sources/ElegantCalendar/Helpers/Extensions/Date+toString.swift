e07e7d513387c21b01964edeb8459ae14e84fa6f
oBlank <dyh1919@gmail.cm>
2023-11-13 22:26:27 +0800

    feat: update

--- Sources/ElegantCalendar/Helpers/Extensions/Date+toString.swift
+++ Sources/ElegantCalendar/Helpers/Extensions/Date+toString.swift
@@ -26,6 +26,16 @@ extension Date {
 
 }
 
+extension Date {
+    func dayOfWeek() -> String? {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "EEEE"
+        return dateFormatter.string(from: self).capitalized
+        // or use capitalized(with: locale) if you want
+    }
+}
+
+
 extension DateFormatter {
 
     static var abbreviatedMonth: DateFormatter {
